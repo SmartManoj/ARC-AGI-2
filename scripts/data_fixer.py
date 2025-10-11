2de1e95f3fb640a0b3a81e9b5dbb63e8d2162833
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2025-10-11 17:12:12 +0530

    e12f9a14

--- scripts/data_fixer.py
+++ scripts/data_fixer.py
@@ -1,12 +1,14 @@
 import json
 from pyperclip import copy
 
-path = r"data\evaluation\88e364bc.json"
+path = r"data\evaluation\e12f9a14.json"
 data = json.load(open(path))
 # INFO: main.py:70 - At row =4, col = 12, expected_value = 4 != actual_value = 0
 # INFO: main.py:70 - At row =8, col = 16, expected_value = 0 != actual_value = 4
 # data['test'][1]['input'][21][4] = 2
-data['test'][0]['output'][4][12] = 0
-data['test'][0]['output'][8][16] = 4
+split = 'train'
+idx = 2
+for row in range(13,19):
+    data[split][idx]['output'][row][row+1] = 4
 with open(path, "w") as f:
     f.write(json.dumps(data))
